c31e424d572485e19eaa592ccfb2653786c0a99a
Bruno Milano <brnmilano.dev@gmail.com>
2026-04-14 10:21:17 -0300

    criação das páginas home e 404 - teste utilizando express

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+require("dotenv").config();
+
 const express = require("express");
+const path = require("path");
+
 const app = express();
-const server = 5000;
+const port = process.env.PORT || 5000;
+const basePath = path.join(__dirname, "templates");
+
+// HABILITA O EXPRESS PARA SERVIR ARQUIVOS ESTÁTICOS DA PASTA "PUBLIC"
+app.use(express.static("public"));
+
+// ROTA PARA SERVIR O ARQUIVO HTML PÁGINA PRINCIPAL
+app.get("/", (req, res) => {
+  res.sendFile(`${basePath}/index.html`);
+});
+
+// ROTA PARA SERVIR O ARQUIVO HTML PÁGINA 404
+app.use((req, res, next) => {
+  res.status(404).sendFile(`${basePath}/404.html`);
+});
+
+// INICIA O SERVIDOR NA PORTA DEFINIDA
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
